86bf5a89b88cb6c1604cf66a9a9cbcaad7e2c327
1iterallyMe <supesupeowo@gmail.com>
2026-01-30 19:03:48 +0300

    Добавлены комментарии

--- Backend/main.py
+++ Backend/main.py
@@ -2,31 +2,40 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
 
+# Даные для входа администратора
+
 data = {
     "Name": "Admin",
     "Password": "Admin123"
 }
 
+# Все модели данных
+
 class Login(BaseModel):
     username: str
     password: str
 
+# Модель для создания поста
 class CreatePost(BaseModel):
     heading: str
     title: str
     photo: str
 
+# Модель для редактирования поста   
 class EditPost(BaseModel):
     post_id: int
     heading: str
     title: str
     photo: str
 
+# Модель для создания работника
 class CreatePerson(BaseModel):
     name: str
     job_title: int
     person_photo: str
 
+
+# Вход и выход из аккаунта
 @app.post("/login",
           tags=["Аккаунты"],
           summary="Вход",)
@@ -44,12 +53,9 @@ def login(credentials: Login):
 def logout():
     return {"message": "Успешный выход"}
 
-@app.get("/post/get_posts",
-          tags=["Посты"],
-          summary="Получение постов",)
-def get_posts():
-    return {"message": "Получение постов"}
+# Создание, редактирование, удаление постов
 
+# Создание поста
 @app.post("/post/create_post",
           tags=["Посты"],
           summary="Создание поста",)
@@ -61,23 +67,30 @@ def create_post(post: CreatePost):
         "message": "Пост успешно создан"
     }
 
+# Редактирование поста
 @app.post("/post/edit_posts", 
           tags=["Посты"],
           summary="Редактирование постов",) 
 def edit_posts(post: EditPost):
-    return {"message": "Редактирование постов"}
+    return {"message": "Пост успешно отредактирован"}
 
+# Получение постов
+@app.get("/post/get_posts",
+          tags=["Посты"],
+          summary="Получение постов",)
+def get_posts():
+    return {"message": "Получение постов"}
+
+# Удаление поста
 @app.delete("/post/delete_post/{post_id}",
              tags=["Посты"],
              summary="Удаление поста",)
 def delete_post(post_id: int):
     return {"message": f"Пост с ID {post_id} успешно удален"}
 
-@app.get("/person/get_persons",
-          tags=["Работники"],
-          summary="Получение работников",)
-def get_persons():
-    return {"message": "Получение работников"}  
+# Создание, редактирование, удаление работников
+
+# Создание работника
 
 @app.post("/person/create_person",
           tags=["Работники"],
@@ -89,12 +102,22 @@ def create_person(person: CreatePerson):
         "person_photo": person.person_photo,
         "message": "Работник успешно создан"
     }
+
+# Редактирование работника
 @app.post("/person/edit_person",
             tags=["Работники"],
             summary="Редактирование работника",)    
 def edit_person(person: CreatePerson):
-    return {"message": "Редактирование работника"}
+    return {"message": "Работник успешно отредактирован"}
 
+# Получение работников
+@app.get("/person/get_persons",
+          tags=["Работники"],
+          summary="Получение работников",)
+def get_persons():
+    return {"message": "Получение работников"}  
+
+# Удаление работника
 @app.delete("/person/delete_person/{person_id}",
              tags=["Работники"],
              summary="Удаление работника",)
